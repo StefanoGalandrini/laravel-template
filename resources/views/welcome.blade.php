c6c45e485529b9bd96943380a822c9b979e2b046
StefanoGalandrini <stefe68@gmail.com>
2023-07-30 13:02:55 +0200

    Add necessary dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,20 +6,43 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>Laravel Template</title>
+	@vite('resources/js/app.js')
 
 	<!-- Fonts -->
 	<link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
 	<!-- Styles -->
 	<style>
-		body {
-			font-family: 'Nunito', sans-serif;
+		.welcome {
+			display: flex;
+			flex-direction: column;
+			width: 100%;
+			height: 100vh;
+			justify-content: start;
+			align-items: center;
+			background-color: rgb(30, 30, 30);
+		}
+
+		img {
+			max-width: 150px;
+			margin: 6rem 0;
+		}
+
+		h1 {
+			color: rgb(200, 0, 0) !important;
+			font-size: 4.5rem !important;
 		}
 	</style>
 </head>
 
 <body>
-	<h1 style="color: maroon; text-align: center;">Laravel Template</h1>
+	<div class="welcome">
+		<div class="logo">
+			<img src="{{ Vite::asset('resources/img/laravel.png') }}" alt="Logo Laravel">
+			<img src="{{ Vite::asset('resources/img/vite-logo.png') }}" alt="Logo Vite">
+		</div>
+		<h1>Laravel-Vite Template</h1>
+	</div>
 </body>
 
 </html>
